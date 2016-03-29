5e981db6921757196cebf37e41873e981a726936
Samuel Ramos <alu0100815146@ull.edu.es>
2016-03-29 12:48:13 +0100

    Iniciando practica karma travis

---
+++ README.md
@@ -0,0 +1,23 @@
+# Práctica de Testing con Mocha, chai, blanket y gulp
+
+Este repositorio aloja un conversor de temperaturas y sus correspondientes test
+
+**Dirección del campus virtual**
+
+* [Enunciado de la práctica](https://campusvirtual.ull.es/1516/mod/page/view.php?id=182938)
+* [Gitbook con el enunciado](https://casianorodriguezleon.gitbooks.io/pl1516/content/mochachaisinon.html)
+
+**Repositorio GitHuB**
+
+* [FORK](https://github.com/Losnen/mocha-y-chai-joshua-samuel)
+* [Repositorio de entrega](https://github.com/ULL-ESIT-GRADOII-DSI/mocha-y-chai-joshua-samuel)
+
+**Página de la práctica**
+
+* [Sitio web de la página](https://ULL-ESIT-GRADOII-DSI.github.io/mocha-y-chai-joshua-samuel)
+* [Testing de la práctica](https://ULL-ESIT-GRADOII-DSI.github.io/mocha-y-chai-joshua-samuel/test)
+
+**Página del autor**
+
+* [Samuel Ramos Barroso](http://losnen.github.io/)
+* [Joshua Pérez](http://joshuape.github.io/)
---
+++ assets/js/medida.js
@@ -0,0 +1,49 @@
+var regexp = XRegExp('^([ ]*) \n' +
+                    '(?<val> [-+]?[0-9]+(\.[0-9]+)?(?:e[+-]?[0-9]+)?) # val \n' +
+                    '([ ]*) \n' +
+                    '(?<tip> [a-zA-Z]) # tip \n' +
+                    '([ ]*) \n' +
+                    '(?<to> (to))? # to \n' +
+                    '([ ]*) \n' +
+                    '(?<para> [a-zA-Z]+) # para \n' +
+                    '([ ]*)$','x');
+ 
+
+
+function Medida (valor,tipo)
+{
+    var val = XRegExp.exec(valor, regexp);
+    if (val) {
+      this.valor = val[1];
+      this.tipo = val[2];
+    } else {
+      this.valor = valor;
+      this.tipo = tipo;
+    }
+}
+
+
+Medida.measures = {};
+
+Medida.convertir = function(valor) {
+
+  var match = XRegExp.exec(valor, regexp);
+
+  if (match) {
+    var numero = match.val,
+        tipo   = match.tip.toLowerCase(),
+        destino = match.para.toLowerCase();
+
+    try {
+      var source = new measures[tipo](numero);                  
+      var target = "to"+measures[destino].name;                
+      return source[target]().toFixed(2) + " "+measures[destino].name;        
+    }
+    catch(err) {
+      console.log(err);
+      return 'Desconozco como convertir desde "'+tipo+'" hasta "'+destino+'"';
+    }
+  }
+  else
+    return "Introduzca una temperatura valida: 330e-1 F to C";
+};
